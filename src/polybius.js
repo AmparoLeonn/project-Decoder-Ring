be5ca5e3f889aeeb1b7f10c37678b8eb43eefa9b
amparo leon <108093085+AmparoLeonn@users.noreply.github.com>
2022-08-14 14:44:55 -0700

    Update polybius.js

--- src/polybius.js
+++ src/polybius.js
@@ -1,11 +1,11 @@
 // Please refrain from tampering with the setup code provided here,
 // as the index.html and test files rely on this setup to work properly.
 // Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
 
 const polybiusModule = (function () {
 
   function polybius(input, encode = true) {
+    //The letters I and J share a space. When encoding, both letters can be converted to 42, but when decoding, both letters should somehow be shown.
     const encoder = {
       a: "11", b: "21", c: "31", d: "41", e: "51", f: "12", g: "22", h: "32", i: "42", j: "42", k: "52", l: "13", m: "23", n: "33", o: "43", p: "53", q: "14", r: "24", s: "34", t: "44", u: "54", v: "15", w: "25", x: "35", y: "45", z: "55",
     };
@@ -14,9 +14,9 @@ const polybiusModule = (function () {
 11: "a", 21: "b", 31: "c", 41: "d", 51: "e", 12: "f", 22: "g", 32: "h", 42: "(i/j)", 52: "k", 13: "l", 23: "m", 33: "n", 43: "o", 53: "p", 14: "q", 24: "r", 34: "s", 44: "t", 54: "u", 15: "v", 25: "w", 35: "x", 45: "y", 55: "z",
     };
 
-    input = input.split("");
+    input = input.split(""); //input refers to the inputted text to be encoded or decoded.
 
-    if (encode) {
+    if (encode) { //encode refers to whether you should encode or decode the message. By default it is set to true.
       return input.reduce((result, char) => {
         char = char.toLowerCase();
         const code = char.charCodeAt();
@@ -26,6 +26,7 @@ const polybiusModule = (function () {
         return `${result}${char}`;
       }, "");
     }
+//When encoding, your output should still be a string.
 
     const result = input.reduce((acc, char) => {
       return char !== " " ? acc + 1 : acc;
